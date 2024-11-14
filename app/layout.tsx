44df0125e1b395bfea3319d66ce2ded3f38e54d4
Microtechbymak26 <usubka786@Gmail.com>
2024-11-14 12:31:57 -0800

    .........

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import { Barlow_Condensed } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "./components/Navigation/ResponsiveNav";
@@ -10,16 +9,6 @@ const barlowCondensed = Barlow_Condensed({
   subsets: ["latin"],
 }) 
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Burger | A healthy Food",
